73ab883a76e9d9cd89020e3180977448d2244ab0
Luis Alejandro Cervantes De Sicilia <aleph_n@desarrollo.space>
2018-06-24 01:44:23 -0500

    Allow all incoming connections

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -50,7 +50,7 @@ resource "aws_route_table_association" "route_association" {
 }
 
 resource "aws_security_group" "security_group_desarrollo" {
-  name        = "sg_desarrollo_service"
+  name        = "sg_desarrollo_services"
   description = "services traffic"
   vpc_id      = "${aws_vpc.vpc.id}"
 
@@ -59,16 +59,9 @@ resource "aws_security_group" "security_group_desarrollo" {
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
